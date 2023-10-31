2bde7ea931b2084f9cd5eb8acccae3c4607dbc09
Natalia Morales <natii.vmorales98@gmail.com>
2023-10-31 12:22:49 -0300

    agregamos ventanas emergentes con alert y prompt, ademas probamos el parseInt

--- js/01-variables.js
+++ js/01-variables.js
@@ -20,3 +20,20 @@ anioActual = 2023 + 1;
 console.log("Año actual: " + anioActual );
 console.log(url);
 
+document.write("<h1 class='titulo'>hola mundo "+anioActual+"</h1>");
+document.write("hola mundo <br>");
+document.write("hola mundo");
+
+//ventana emergente
+alert("Esto es un ejemplo de una alerta");
+
+//solicitar un número al usuario y luego sumarle 5, por ultimo mostrar el resultado de la suma.
+const numeroSolicitado = parseInt(prompt("Ingrese un número del 1 al 100"));
+console.log(numeroSolicitado);
+
+console.log("Resultado: "+ (numeroSolicitado + 5));
+console.log(numeroSolicitado + 5);
+
+//transformar o parsear un número de string a formato numerico
+//parseInt("100");
+//parseFloat("100.50");
