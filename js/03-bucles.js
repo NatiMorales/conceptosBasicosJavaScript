fae5e5ced4fedc6e631fcc3ad7d9010e3e9ba011
Natalia Morales <natii.vmorales98@gmail.com>
2023-10-31 14:05:06 -0300

    agregamos un ejemplo de bucle for

--- js/03-bucles.js
+++ js/03-bucles.js
@@ -25,3 +25,14 @@ do{
     document.write(`<p>Renglon número ${contador} con do while</p>`);
     contador--;
 }while(contador >= 1)
+
+/*
+for(inicializo una variable; condicion logica; incremento o decremento){
+    todas las lineas de codigo que quiero repetir varias veces 
+    agregar un codigo para que la condicion no se cumpla en algún momento
+}
+*/
+
+for(let renglon = 1; renglon <= 10; renglon++){
+    document.write(`<p>Renglon número ${renglon} con for</p>`);
+}
