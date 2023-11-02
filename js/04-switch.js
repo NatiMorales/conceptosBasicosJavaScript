26cbf7224d652dce9f8085432372879b97f4feac
Natalia Morales <natii.vmorales98@gmail.com>
2023-11-02 13:49:49 -0300

    se agregaron modificaciones en el ejercicio del cajero y se lo reemplazo por un switch

--- js/04-switch.js
+++ js/04-switch.js
@@ -1,32 +1,69 @@
 // si el usuario presiona 1 consulta el sueldo, 2 extraer dinero, 3 ingresar dinero;
-
 let saldo = 1000;
+const opcion = prompt("seleccione una opción: 1- Consultar el saldo, 2- Extraer dinero, 3- Ingresar dinero");
 
-const opcion = parseInt(prompt("seleccione una opción: 1- Consultar el saldo, 2- Extraer dinero, 3- Ingresar dinero"));
+// if (opcion === 1) {
+//   //console.log("debo mostrar el saldo");
+//   console.log("Saldo disponible: " + saldo);
+// } else if (opcion === 2) {
+//   //console.log("extraer dinero");
+//   const montoAExtraer = parseFloat(prompt("Ingrese el monto a extraer"));
+//   if (montoAExtraer > 0 && montoAExtraer <= saldo) {
+//     saldo = saldo - montoAExtraer;
+//     console.log("EXTRACCIÓN EXITOSA");
+//     console.log("Saldo disponible: " + saldo);
+//     document.write("EXTRACCIÓN EXITOSA");
+//     document.write("Saldo disponible: " + saldo);
+//   } else {
+//     alert("Ingrese un monto mayor a 0 (cero)");
+//   }
+// } else if (opcion === 3) {
+//   //console.log("ingresar dinero");
+//   const montoAIngresar = parseFloat(prompt("Ingrese el monto a agregar"));
+//   if (montoAIngresar > 0) {
+//     saldo = saldo + montoAIngresar;
+//     console.log("INGRESO EXITOSO");
+//     console.log("Saldo disponible: " + saldo);
+//     document.write("INGRESO EXITOSO");
+//     document.write("Saldo disponible: " + saldo);
+//   } else {
+//     alert("Ingrese un monto mayor a 0 (cero)");
+//   }
+// } else {
+//   alert("Ingresó una opción inválida");
+// }
 
-if(opcion === 1){
-    //console.log("debo mostrar el saldo");
-    console.log("Saldo disponible: "+ saldo);
-} else if(opcion  === 2){
-    //console.log("extraer dinero");
+switch (opcion) {
+  case "saldo":
+  case "consultar saldo":
+  case "1":
+    console.log("Saldo disponible: " + saldo);
+    document.write("Saldo disponible: " + saldo);
+    break;
+  case "2":
     const montoAExtraer = parseFloat(prompt("Ingrese el monto a extraer"));
-    if(montoAExtraer > 0){
-        saldo = saldo - montoAExtraer;
-        console.log("EXTRACCIÓN EXITOSA");
-        console.log("Saldo disponible: "+ saldo);
-    }else{
-        console.log("Ingrese un monto mayor a 0 (cero)");
+    if (montoAExtraer > 0 && montoAExtraer <= saldo) {
+      saldo = saldo - montoAExtraer;
+      console.log("EXTRACCIÓN EXITOSA");
+      console.log("Saldo disponible: " + saldo);
+      document.write("EXTRACCIÓN EXITOSA");
+      document.write("Saldo disponible: " + saldo);
+    } else {
+      alert("Ingrese un monto mayor a 0 (cero)");
     }
-} else if(opcion === 3){
-    //console.log("ingresar dinero");
+    break;
+  case "3":
     const montoAIngresar = parseFloat(prompt("Ingrese el monto a agregar"));
-    if(montoAIngresar > 0){
-        saldo = saldo + montoAIngresar;
-        console.log("INGRESO EXITOSO");
-        console.log("Saldo disponible: "+ saldo);
-    }else{
-        console.log("Ingrese un monto mayor a 0 (cero)");
+    if (montoAIngresar > 0) {
+      saldo = saldo + montoAIngresar;
+      console.log("INGRESO EXITOSO");
+      console.log("Saldo disponible: " + saldo);
+      document.write("INGRESO EXITOSO");
+      document.write("Saldo disponible: " + saldo);
+    } else {
+      alert("Ingrese un monto mayor a 0 (cero)");
     }
-} else {
-    console.log("Ingresó una opción inválida");
+    break;
+  default:
+    alert("Ingresó una opción inválida");
 }
