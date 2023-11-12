5fa77220d88a0d86685ead799cf98b7a744717a7
Natalia Morales <natii.vmorales98@gmail.com>
2023-11-12 18:07:53 -0300

    agregamos las funciones en forma de flecha

--- js/06-funciones.js
+++ js/06-funciones.js
@@ -8,11 +8,21 @@ function saludoNuevo(nombreIngresado, apellidoIngresado){
     document.write(`<p>Hola ${nombreIngresado}, ${apellidoIngresado}. Que tengas un buen día</p>`)
 }
 
-function sumar(numero1, numero2){
-    const resultado = numero1 + numero2;
-    console.log(resultado);
-    return resultado;
-}
+// function sumar(numero1, numero2){
+//     const resultado = numero1 + numero2;
+//     console.log(resultado);
+//     return resultado;
+// }
+
+//arrow functions
+// const sumar = (numero1, numero2) => {
+//     const resultado = numero1 + numero2;
+//     console.log(resultado);
+//     return resultado;
+// }
+
+const sumar = (numero1, numero2) => numero1 + numero2;
+
 
 //invocar o llamar una función
 saludar();
@@ -27,3 +37,4 @@ saludoNuevo("Batman");
 const valorResultante = sumar(30,28);
 document.write(`<p>El resultado de la suma es: ${valorResultante}</p>`);
 document.write(`<p>El resultado de la suma es: ${sumar(20,30)}</p>`);
+
